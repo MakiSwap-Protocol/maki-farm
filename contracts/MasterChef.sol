2d28812612b4217f762762e74fe1e1c525ad0aa5
Uni <cryptounico@gmail.com>
2021-04-21 10:37:58 -0500

    add: IMigratorChef

--- contracts/MasterChef.sol
+++ contracts/MasterChef.sol
@@ -9,7 +9,19 @@ import 'maki-swap-lib/contracts/access/Ownable.sol';
 
 import "./MakiToken.sol";
 import "./SoyBar.sol";
-import './interfaces/IMigratorChef.sol';
+
+interface IMigratorChef {
+    // Perform LP token migration from legacy EasyBakeSwap.
+    // Take the current LP token address and return the new LP token address.
+    // Migrator should have full access to the caller's LP token.
+    // Return the new LP token address.
+    //
+    // XXX Migrator must have allowance access to DOUGH LP tokens.
+    // EasyBakeSwap must mint EXACTLY the same amount of DOUGH LP tokens or
+    // else something bad will happen. Traditional EasyBakeSwap does not
+    // do that so be careful!
+    function migrate(IERC20 token) external returns (IERC20);
+}
 
 // MasterChef is the master of Maki. She can make Maki and she is a fair lady.
 //
